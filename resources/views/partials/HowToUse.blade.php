dbade08a725ef36db28335b32d4af753e45a3fb8
ikumagai <ittsu.2060@ezweb.ne.jp>
2019-11-26 16:51:32 +0900

    first commit

--- resources/views/partials/HowToUse.blade.php
+++ resources/views/partials/HowToUse.blade.php
@@ -6,7 +6,7 @@
         <div class="use_contents">        
                 <img src="../../LPImages/phone image 1 .png" alt="register page image">
             <div class="Description">
-                <h3>Register</h3>
+                <h3><span style="color: #F70661; margin-right: 1rem;">●</span>Register</h3>
                 <p>Register or login to use it more comfortably.</p>
                 <div class="register-btn">
                     <a href="/register">Register</a>
@@ -17,7 +17,7 @@
         @endif
         <div class="use_contents use_contents2">           
             <div class="Description" style="align-items: center;">
-                <h3>Create a profile</h3>
+                <h3><span style="color: #F70661; margin-right: 1rem;">●</span>Create a profile</h3>
                 <p>Create your profile and be part of the community </p>
             </div>
             <img src="../../LPImages/phone image 2.png" alt="profile page image">
@@ -26,10 +26,10 @@
         <div class="use_contents">        
             <img src="../../LPImages/phone image 3@2x.png" alt="event search page image">
                 <div class="Description">
-                    <h3>Find events</h3>
-                    <p><span style="color: #F70661; margin-right: 1rem;">●</span>Save your favorite events</p>
+                    <h3><span style="color: #F70661; margin-right: 1rem;">●</span>Find events</h3>
+                    <p>Save your favorite events</p>
                         
-                    <p><span style="color: #F70661; margin-right: 1rem;">●</span>Look at recommended events</p>
+                    <p>Look at recommended events</p>
                            
                 </div> 
         </div>
